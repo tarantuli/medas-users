ab70a5bdbe1144134e552169740243e6b4c9cb17
Patrick Morpey <patrick@morphp.nl>
2026-05-18 13:23:14 +0200

    Simplified imports in `PackageEntities` and removed unused dependencies from `composer.json` and `composer.lock`.

--- src/PackageEntities.php
+++ src/PackageEntities.php
@@ -4,8 +4,7 @@ declare(strict_types=1);
 
 namespace Medas\Users;
 
-use Medas\Core\Attributes\Service;
-use Medas\StorageManager\Interfaces\PackageEntities as PackageEntitiesInterface;
+use Medas\Core\{Attributes\Service, Interfaces\PackageEntities as PackageEntitiesInterface};
 
 #[Service]
 readonly class PackageEntities implements PackageEntitiesInterface
